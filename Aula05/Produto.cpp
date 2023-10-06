8439645e9cb4301441386daa5a4d48cc819fb5bf
IndeoCore <jpiflorencio@gmail.com>
2023-10-06 03:09:25 -0300

    ⚡️ Aula05_Atrasada

---
+++ Aula05/Produto.cpp
@@ -0,0 +1,26 @@
+#include "Produto.h"
+
+#include <iostream>
+
+using namespace std;
+
+// IMPLEMENTAR CONSTRUTOR
+
+Produto::Produto(string nome, double preco) : nome(nome), preco(preco) {}
+
+Produto::~Produto(){
+    cout << "Produto " << this->getNome() << " destruido" << endl;
+}
+
+string Produto::getNome() {
+    return nome;
+}
+
+double Produto::getPreco() {
+    return preco;
+}
+
+void Produto::imprimir(){
+    cout << nome << " - " << preco << " reais cada" << endl;
+}
+
